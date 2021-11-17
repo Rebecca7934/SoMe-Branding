bf4ab938e3f75f0aac037f626575d1740bbcf4cf
Rebecca <rebeccaosterby@hotmail.com>
2021-11-17 13:11:33 +0100

    jave til burgermenu

---
+++ burgermenu.js
@@ -0,0 +1,7 @@
+function toggleMobile()
+{
+  var mobileUl = document.getElementById("main-header-ul");
+  var hasClass = (mobileUl.className != "");
+  var newClass = (hasClass ? "" : "show");
+  mobileUl.className = newClass;
+}
